c9177b5c799878c0872e55e6de02107a2ff59fe0
Joel Teichroeb <joel@teichroeb.net>
2013-06-28 16:01:26 -0700

    Readd the current year to the page

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,6 +1,6 @@
       <div id="footer">
         <p>
-          Copyright &copy; <a href="mailto:csss-exec@sfu.ca">Computing Science Student Society</a> 2009-2012
+          Copyright &copy; <a href="mailto:csss-exec@sfu.ca">Computing Science Student Society</a> 2009-<?php echo date("Y"); ?>
         </p>
       </div>
     </div>
