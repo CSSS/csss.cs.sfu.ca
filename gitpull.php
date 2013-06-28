a29bb32208391bb13e3b11927468f8e6f83fb06f
Joel Teichroeb <joel@teichroeb.net>
2013-06-28 16:58:03 -0700

    Forgot the json part

---
+++ gitpull.php
@@ -0,0 +1,11 @@
+<?php 
+@include('includes/sessions.inc.php');
+header('Content-Type: application/json');
+echo '{"status_msg":"';
+echo rtrim(`git pull`);
+echo '<br />';
+echo rtrim(`git submodule update`);
+echo '", "sha":"';
+echo rtrim(`git rev-parse HEAD`);
+echo '"}';
+?>
