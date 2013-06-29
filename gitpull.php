5d0f6155add1c53ee5939c5bdaf19823f1554a4b
Joel Teichroeb <joel@teichroeb.net>
2013-06-28 17:37:35 -0700

    Add log information for git pulls

--- gitpull.php
+++ gitpull.php
@@ -1,5 +1,6 @@
 <?php 
 @include('includes/sessions.inc.php');
+writeToLog($dbSession, $SESSION['username'], $SESSION['ip'], 'Pulled from git');
 header('Content-Type: application/json');
 echo '{"status_msg":"';
 echo rtrim(`git pull`);
