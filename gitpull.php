7c7e4c9b59a74b2fac42708168571d803d5db99d
Joel Teichroeb <joel@teichroeb.net>
2013-06-28 17:41:23 -0700

    Log the new version too

--- gitpull.php
+++ gitpull.php
@@ -2,11 +2,16 @@
 @include('includes/sessions.inc.php');
 writeToLog($dbSession, $SESSION['username'], $SESSION['ip'], 'Pulled from git');
 header('Content-Type: application/json');
+$rev = `git rev-parse HEAD`;
 echo '{"status_msg":"';
 echo rtrim(`git pull`);
 echo '<br />';
 echo rtrim(`git submodule update`);
 echo '", "sha":"';
-echo rtrim(`git rev-parse HEAD`);
+$newrev = `git rev-parse HEAD`;
+echo rtrim($newrev);
 echo '"}';
+if ($newrev != $rev) {
+	writeToLog($dbSession, $SESSION['username'], $SESSION['ip'], 'Git now at ' . `git rev-parse --short HEAD`);
+}
 ?>
